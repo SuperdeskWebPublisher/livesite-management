abab3436e60e0ec3e9920386e6435c5486d8799e
pavlovicnemanja <me@pavlovicnemanja.com>
2017-01-09 17:50:01 +0100

    feat(core): Initial structure

---
+++ app/index.ts
@@ -0,0 +1,6 @@
+import './polyfills';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './components/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
