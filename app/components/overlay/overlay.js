a3a9f29d02a11b9d2d17d4714f777710653fb9ff
Tomasz Rondio <tomasz.rondio@gmail.com>
2018-10-10 15:25:50 +0200

    fix: styling

--- app/components/overlay/overlay.js
+++
@@ -1,8 +0,0 @@
-function overlay() {
-    return {
-        templateUrl: 'app/components/overlay/overlay.html',
-    };
-}
-
-angular.module('livesite-management.components.overlay', [])
-        .directive('overlay', overlay);
